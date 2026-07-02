924dcc689bbd70771100cacbab10217582d8c919
andresxDDDD <moraga_2006+1@hotmail.com>
2026-07-02 17:22:07 +0000

    avance

--- ORM/urls.py
+++ ORM/urls.py
@@ -5,7 +5,11 @@ from . import views
 
 # el requests  llega desde el router aca y cada ruta gatilla un funcion 
 urlpatterns = [
-   
-    path("lista/",views.lista_libros, name="lista_libros"),
-    path("consulta/",views.libro_gabriel, name="libro_gabriel"),
+
+
+    path('', views.index, name='index'),
+    path('recuperacion/', views.vista_recuperacion, name='recuperacion'),
+    path('filtros/', views.vista_filtros, name='filtros_exclusiones'),
+    path('sql/', views.vista_sql, name='consultas_sql'),
+    path('anotaciones/', views.vista_anotaciones, name='campos_anotaciones'),
 ]
--- ORM/views.py
+++ ORM/views.py
@@ -4,24 +4,54 @@ from django.shortcuts import render
 from django.shortcuts import render
 from .models import Libro
 
+def index(request):
+    
+    return render(request, 'index.html')
+
 def lista_libros(request):
     libros = Libro.objects.all()
     return render(request, 'lista.html', {'libros': libros})
 
 
-def libro_gabriel(request):
 
-    # Recupera solo los libros cuyo autor sea "Gabriel García Márquez".
+
+# 1. Vista principal que ya creamos
+def index(request):
+    return render(request, 'index.html')
+
+# 2. Vista para Recuperación de registros
+from django.shortcuts import render
+from .models import Libro  # Asegúrate de importar tu modelo Libro
+
+def vista_recuperacion(request):
+
+    libros = Libro.objects.all()
+
+    # 1. Libros de Gabriel García Márquez
     libro = Libro.objects.filter(autor="Gabriel García Márquez")
-   
 
-    # Recupera los libros que tienen más de 200 páginas.
-
+    # 2. Libros con más de 200 páginas
     pagina = Libro.objects.filter(paginas__gt=200)
 
-    #Aplica un filtro para mostrar solo libros disponibles.
+    
 
-    disponibilidad= Libro.objects.filter(disponible=True)
+    # Enviamos todo al template
+    return render(request, 'recuperacion.html', {
+        'libro': libro,
+        'pagina': pagina,
+        'libros': libros,
+    })
+# 3. Vista para Filtros y exclusiones
+def vista_filtros(request):
 
+    # 3. Aplica un filtro para mostrar solo libros disponibles
+   # disponibilidad = Libro.objects.filter(disponible=True)
+    return render(request, 'filtros.html')
 
-    return render(request,'consultas.html', {'libro':libro,'pagina':pagina,'dispo':disponibilidad})
+# 4. Vista para Consultas personalizadas con SQL
+def vista_sql(request):
+    return render(request, 'sql.html')
+
+# 5. Vista para Campos específicos y anotaciones
+def vista_anotaciones(request):
+    return render(request, 'anotaciones.html')
